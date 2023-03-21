a26109eaebfe8879f7519e75d299ed5b802065a5
Emiliano Castor <castoremi12@gmail.com>
2023-03-21 19:55:39 -0300

    Agregar header y footer en carpeta fragment

---
+++ fragment/footer.php
@@ -0,0 +1,10 @@
+<footer class="fixed-bottom">
+    <div class="row py-3">
+        <div class="col-md-2 mx-auto text-center">
+            <a href="https://emicastor.com.ar/agencia" class="d-flex flex-column align-items-center text-decoration-none logo-footer">
+                <span class="text-dark">Emiliano Castor</span>
+                <span class="px-1 bg-dark text-white fw-semibold py-1" style="font-size: 10px;">DESARROLLADOR WEB</span>
+            </a>
+        </div>
+    </div>
+</footer>
