640b4e11ed0ffc5bc43a5e3308711e157ca4a320
Emiliano Castor <66658513+emicastor@users.noreply.github.com>
2023-03-21 20:05:19 -0300

    Agregar la bd .sql

--- simple-crud-poo-php8.sql
+++ simple-crud-poo-php8.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `php-poo`
+-- Base de datos: `simple-crud-poo-php8`
 --
 
 -- --------------------------------------------------------
